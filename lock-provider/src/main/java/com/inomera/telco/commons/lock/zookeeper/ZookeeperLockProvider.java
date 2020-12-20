32b5a17a7fec54b27c6bebcc889f566e0dfec214
Turgay Can <turgay.can@inomera.com>
2020-12-21 01:06:43 +0300

    Add Zookeeper provider. LP-8

--- lock-provider/src/main/java/com/inomera/telco/commons/lock/zookeeper/ZookeeperLockProvider.java
+++ lock-provider/src/main/java/com/inomera/telco/commons/lock/zookeeper/ZookeeperLockProvider.java
@@ -3,18 +3,14 @@ package com.inomera.telco.commons.lock.zookeeper;
 import com.inomera.telco.commons.lock.BaseLockProvider;
 import com.inomera.telco.commons.lock.LockProvider;
 import com.inomera.telco.commons.lock.Locked;
-import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessLock;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
-import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.io.File;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Supplier;
 
 /**
  * Use to lock in a distributed environment
@@ -53,11 +49,9 @@ public class ZookeeperLockProvider extends BaseLockProvider implements LockProvi
 
     @Override
     public Locked lock(String lockMap, String lockKey) {
-
-        InterProcessLock lock = new InterProcessMutex(curatorClient, lockMap);
+        InterProcessLock lock = getMutex(lockMap, lockKey);
         try {
             lock.acquire();
-        } catch (Exception ex) {
         } finally {
             return () -> {
                 try {
@@ -70,8 +64,7 @@ public class ZookeeperLockProvider extends BaseLockProvider implements LockProvi
 
     @Override
     public Optional<Locked> tryLock(String lockMap, String lockKey) {
-        InterProcessLock lock = new InterProcessMutex(curatorClient, lockMap);
-
+        InterProcessLock lock = getMutex(lockMap, lockKey);
         try {
             if (lock.acquire(DEFAULT_LOCK_WAIT_TIME_MS, TimeUnit.MILLISECONDS)) {
                 return Optional.of(() -> {
@@ -83,7 +76,7 @@ public class ZookeeperLockProvider extends BaseLockProvider implements LockProvi
             } else {
                 return Optional.empty();
             }
-        } catch (Exception ex) {
+        } catch (Exception e) {
             return Optional.empty();
         }
     }
@@ -98,55 +91,8 @@ public class ZookeeperLockProvider extends BaseLockProvider implements LockProvi
         return tryLock(defaultLockPath, lockName);
     }
 
-    public static void main(String[] args) {
-        String hosts = "localhost:2181";
-        int baseSleepTimeMills = 1000;
-        int maxRetries = 3;
-        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMills, maxRetries);
-        CuratorFramework client = CuratorFrameworkFactory.newClient(hosts, retryPolicy);
-        try {
-            client.start();
-
-            if (doStuff(client)) {
-                return;
-            }
-        } finally {
-            if (client != null) {
-                client.close();
-            }
-            System.exit(0);
-        }
-    }
-
-    private static boolean doStuff(CuratorFramework client) {
-        //Zookeeper version :  3.5.8
-        LockProvider lockProvider = new ZookeeperLockProvider(client);
-        final Locked locked = lockProvider.lock("zoo1");
-        if (locked != null) {
-            System.out.println("zoo1 Locked acquired!!");
-            LockProvider lockProvider11 = new ZookeeperLockProvider(client);
-            final Optional<Locked> locked2Instance = lockProvider11.tryLock("zoo1");
-            if (!locked2Instance.isPresent()) {
-                System.out.println("zoo1 Locked cannot acquired!");
-            }
-            locked.unlock();
-            System.out.println("zoo1 Locked is released");
-            final Optional<Locked> locked3 = lockProvider.tryLock("zoo1");
-            if (locked3.isPresent()) {
-                System.out.println("zoo1 Locked is acquired!");
-            }
-            LockProvider lockProvider2 = new ZookeeperLockProvider(client, "/locks2");
-            final Optional<Locked> zoo1DiffPathLocked = lockProvider2.tryLock("zoo1");
-            if (zoo1DiffPathLocked.isPresent()) {
-                System.out.println("zoo1 is Locked acquired with diff path!");
-            }
-            zoo1DiffPathLocked.get().unlock();
-            locked3.get().unlock();
-            System.exit(0);
-            return true;
-        }
-        System.out.println("Cannot locked");
-        return false;
+    private InterProcessLock getMutex(String lockMap, String lockKey) {
+        return new InterProcessMutex(curatorClient, lockMap.concat(File.separator).concat(lockKey));
     }
 
 }
--- lock-provider/src/test/java/com/inomera/telco/commons/lock/zookeeper/ZookeeperLockProviderTest.java
+++ lock-provider/src/test/java/com/inomera/telco/commons/lock/zookeeper/ZookeeperLockProviderTest.java
@@ -1,7 +1,344 @@
 package com.inomera.telco.commons.lock.zookeeper;
 
+import com.inomera.telco.commons.lock.Locked;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.test.TestingServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZookeeperLockProviderTest {
+
+    private static final String DEFAULT_PATH = "/tmp/locks";
+
+    private ZookeeperLockProvider lockProvider;
+
+    TestingServer zkServer;
+    CuratorFramework curatorClient;
+
+    @BeforeEach
+    void init() throws Exception {
+        zkServer = new TestingServer(2181, new File(DEFAULT_PATH));
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        curatorClient = CuratorFrameworkFactory.newClient(zkServer.getConnectString(), retryPolicy);
+        curatorClient.start();
+        lockProvider = new ZookeeperLockProvider(curatorClient, DEFAULT_PATH);
+    }
+
+    @AfterEach
+    void end() throws IOException {
+        curatorClient.close();
+        zkServer.stop();
+    }
+
+    @Test
+    @DisplayName("Constructor without default path name argument should initialize a default path name")
+    void constructorWithoutDefaultMapName_shouldInitializeADefaultMapName() {
+        final Locked locked = lockProvider.lock("aLock");
+        assertNotNull(locked);
+        locked.unlock();
+    }
+
+    @Test
+    @DisplayName("lock with custom path and lockKey should unlock on a path")
+    void lock_shouldUnlockOnMap() {
+        final Locked locked = lockProvider.lock("/tmp/locks2", "aLock");
+
+        assertNotNull(locked);
+
+        locked.unlock();
+    }
+
+    @Test
+    @DisplayName("tryLock with default path and lockKey should lock on a path")
+    void tryLock_shouldTryLockOnMap_returnFullOptional() {
+        final Optional<Locked> locked = lockProvider.tryLock("aLock");
+
+        assertNotNull(locked);
+        assertTrue(locked.isPresent());
+        locked.get().unlock();
+    }
+
+    @Test
+    @DisplayName("tryLock with custom path and lockKey should unlock on a path")
+    void tryLock_shouldUnlockOnMap() {
+
+        final Optional<Locked> locked = lockProvider.tryLock("/tmp/locks2", "aLock");
+
+        assertNotNull(locked);
+        assertTrue(locked.isPresent());
+        locked.get().unlock();
+    }
+
+    @Test
+    @DisplayName("tryLock with custom path and lockKey should return empty Optional when lock is owned")
+    void tryLock_shouldTryLockOnMap_returnEmptyOptional() {
+        final Locked aLock = lockProvider.lock("/tmp/locks2", "aLock");
+        final Optional<Locked> locked = lockProvider.tryLock("/tmp/locks2", "aLock");
+        assertNotNull(locked);
+        assertFalse(locked.isPresent());
+        aLock.unlock();
+    }
+
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper path and unlock after task finishes")
+    void executeInLock_shouldLockInMapAndUnlockAfterRunnableFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        lockProvider.executeInLock("/tmp/locks2", "aLock", () -> {
+            counter.incrementAndGet();
+            final Optional<Locked> lockedOptional = lockProvider.tryLock("/tmp/locks2", "aLock");
+            assertNotNull(lockedOptional);
+            assertFalse(lockedOptional.isPresent());
+        });
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper path and unlock after task throws exception")
+    void executeInLock_shouldLockInMapAndUnlockAfterRunnableThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInLock("/tmp/locks2", "aLock", (Runnable) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+        final Optional<Locked> lockedOptional = lockProvider.tryLock("/tmp/locks2", "aLock");
+        assertNotNull(lockedOptional);
+        assertTrue(lockedOptional.isPresent());
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper path and unlock after task finishes and return the value")
+    void executeInLock_shouldLockInMapAndUnlockAfterSupplierFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        int returnValue = lockProvider.executeInLock("/tmp/locks2", "aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+
+        assertEquals(1, counter.get());
+        assertEquals(3, returnValue);
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper path and unlock after supplier throws exception")
+    void executeInLock_shouldLockInMapAndUnlockAfterSupplierThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInLock("/tmp/locks2", "aLock", (Supplier<String>) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper default path and unlock after task finishes")
+    void executeInLock_shouldLockAndUnlockAfterRunnableFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        lockProvider.executeInLock("aLock", () -> {
+            counter.incrementAndGet();
+        });
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper default path and unlock after task throws exception")
+    void executeInLock_shouldLockAndUnlockAfterRunnableThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInLock("aLock", (Runnable) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper default path and unlock after task finishes and return the value")
+    void executeInLock_shouldLockAndUnlockAfterSupplierFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        int returnValue = lockProvider.executeInLock("aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+
+        assertEquals(1, counter.get());
+        assertEquals(3, returnValue);
+    }
+
+    @Test
+    @DisplayName("executeInLock should lock in Zookeeper default path and unlock after supplier throws exception")
+    void executeInLock_shouldLockAndUnlockAfterSupplierThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInLock("aLock", (Supplier<String>) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper path and unlock after task finishes")
+    void executeInTryLock_shouldLockInMapAndUnlockAfterRunnableFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        lockProvider.executeInTryLock("/tmp/locks2", "aLock", () -> {
+            counter.incrementAndGet();
+        });
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper path and unlock after task throws exception")
+    void executeInTryLock_shouldLockInMapAndUnlockAfterRunnableThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInTryLock("/tmp/locks2", "aLock", (Runnable) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should not lock in Zookeeper path and unlock when tryLock returns false")
+    void executeInTryLock_shouldNotLockInMapAndUnlockWhenTryLockReturnsFalse() {
+        final Locked lock = lockProvider.lock("/tmp/locks2", "aLock");
+        final AtomicInteger counter = new AtomicInteger(0);
+
+        lockProvider.executeInTryLock("/tmp/locks2", "aLock", (Runnable) counter::incrementAndGet);
+        lock.unlock();
+
+        assertEquals(0, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper path and unlock after task finishes and return the value")
+    void executeTryInLock_shouldLockInMapAndUnlockAfterSupplierFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        int returnValue = lockProvider.executeInTryLock("/tmp/locks2", "aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+
+        assertEquals(1, counter.get());
+        assertEquals(3, returnValue);
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper path and unlock after supplier throws exception")
+    void executeInTryLock_shouldLockInMapAndUnlockAfterSupplierThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInTryLock("/tmp/locks2", "aLock", (Supplier<String>) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should not lock in Zookeeper path and unlock when lock is not available")
+    void executeTryInLock_shouldLockInMapAndUnlockWhenLockIsNotAvailable() {
+        final Locked lock = lockProvider.lock("/tmp/locks2", "aLock");
+        final AtomicInteger counter = new AtomicInteger(0);
+        Integer returnValue = lockProvider.executeInTryLock("/tmp/locks2", "aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+        lock.unlock();
+
+        assertEquals(0, counter.get());
+        assertNull(returnValue);
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper and unlock after task finishes")
+    void executeInTryLock_shouldLockAndUnlockAfterRunnableFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        lockProvider.executeInTryLock("aLock", () -> {
+            counter.incrementAndGet();
+        });
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper and unlock after task throws exception")
+    void executeInTryLock_shouldLockAndUnlockAfterRunnableThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInTryLock("aLock", (Runnable) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should not lock in Zookeeper and unlock when the lock is not available")
+    void executeInTryLock_shouldNotLockAndUnlockWhenLockIsNotAvailable() {
+        final Locked lock = lockProvider.lock("aLock");
+        final AtomicInteger counter = new AtomicInteger(0);
+        lockProvider.executeInTryLock("aLock", (Runnable) counter::incrementAndGet);
+        lock.unlock();
+        assertEquals(0, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper and unlock after task finishes and return the value")
+    void executeInTryLock_shouldLockAndUnlockAfterSupplierFinishes() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        int returnValue = lockProvider.executeInTryLock("aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+
+        assertEquals(1, counter.get());
+        assertEquals(3, returnValue);
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should lock in Zookeeper and unlock after supplier throws exception")
+    void executeInTryLock_shouldLockAndUnlockAfterSupplierThrowsException() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        assertThrows(RuntimeException.class, () -> lockProvider.executeInTryLock("aLock", (Supplier<String>) () -> {
+            counter.incrementAndGet();
+            throw new RuntimeException("test");
+        }));
+        assertEquals(1, counter.get());
+    }
+
+    @Test
+    @DisplayName("executeInTryLock should not lock in Zookeeper and unlock when lock is not available and return null")
+    void executeInTryLock_shouldNotLockAndUnlockWhenLockIsNotAvailableAndReturnNull() {
+        final Locked lock = lockProvider.lock("aLock");
+
+        final AtomicInteger counter = new AtomicInteger(0);
+        final Integer returnValue = lockProvider.executeInTryLock("aLock", () -> {
+            counter.incrementAndGet();
+            return 3;
+        });
+
+        lock.unlock();
+        assertEquals(0, counter.get());
+        assertNull(returnValue);
+    }
 
 }
